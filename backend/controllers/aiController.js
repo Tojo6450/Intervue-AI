d78e51355e11d848852827b0ea0990ea55e63f51
Your Name <supriyoghorui645@gmail.com>
2025-06-25 17:19:43 +0530

    intro to gemini api

---
+++ backend/controllers/aiController.js
@@ -0,0 +1,35 @@
+const {GoogleGenAI} = require("@google/genai");
+const {conceptExplainPrompt} = require("../utils/prompts");
+const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+
+//generate interview questions ans answers using gemini
+const generateInterviewQuestions = async(res,res)=>{
+    try{
+       const {role, experience,topicstoFocus,numberofQuestions} = req.body;
+
+       if(!role || !experience || !topicstoFocus || !numberofQuestions){
+        return res.status(400).json({message:"Missing required fields"});
+
+       const prompt = questionAnswerPrompt(role,experience,topicstoFocus,numberofQuestions);
+       const response = await ai.models.generateContent({
+        model:"gemini-2.0-flash-lite",
+        contents:prompt,
+       });
+
+       let rawText = response.text;
+       const cleanedText = rawText.replace(/^```json\s*/,"")
+       .replace(/```$/,"").trim();
+       }
+
+       const data = JSON.parse(cleanedText);
+       res.status(200).json(data);
+    }
+    catch(error){
+       res.status(500).json({message:"Failed to generate questions",error:error.message});
+    }
+}
+
+const generateConceptExplanation = async(req,res)=>{   
+};
+
+module.exports = {generateConceptExplanation,generateInterviewQuestions};
